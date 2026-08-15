ec2bcafb15d666934f03dba5d68e2d9168681f3f
RaviRajChoubey <ravirajchoubey.smn@gmail.com>
2026-08-15 19:08:08 +0530

    Time: 0 ms (100%), Space: 14.1 MB (59.58%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,38 +1,29 @@
 #include <vector>
+using namespace std;
 
 class Solution {
 private:
-    void backtrack(int index, int target, std::vector<int>& candidates, std::vector<int>& current, std::vector<std::vector<int>>& result) {
-        // Base Case 1: Target reached, add combination to result
-        if (target == 0) {
-            result.push_back(current);
+    void calc(int ind,int target,vector<int>& candidates,vector<vector<int>>& ans,vector<int>& a) {
+        if(ind==candidates.size()) {
+            if(target==0) {
+                ans.push_back(a);
+            }
             return;
         }
 
-        // Base Case 2: Out of bounds or target exceeded
-        if (target < 0 || index >= candidates.size()) {
-            return;
+        if(target >= candidates[ind]) {
+            a.push_back(candidates[ind]);
+            calc(ind,target-candidates[ind],candidates,ans,a);
+            a.pop_back();
         }
-
-        // Choice 1: Include the current element candidates[index]
-        // Stay at 'index' because the same element can be reused unlimited times
-        current.push_back(candidates[index]);
-        backtrack(index, target - candidates[index], candidates, current, result);
-        
-        // Backtrack: Undo the choice to explore the skip branch
-        current.pop_back();
-
-        // Choice 2: Skip the current element and move to the next index
-        backtrack(index + 1, target, candidates, current, result);
+        calc(ind+1,target,candidates,ans,a);
     }
 
 public:
-    std::vector<std::vector<int>> combinationSum(std::vector<int>& candidates, int target) {
-        std::vector<std::vector<int>> result;
-        std::vector<int> current;
-        
-        backtrack(0, target, candidates, current, result);
-        
-        return result;
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> a;
+        calc(0,target,candidates,ans,a);
+        return ans;
     }
 };
